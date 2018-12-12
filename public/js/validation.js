3d42b04b838c7eb15ac8425001668fa977904213
Whooolia <tpdms2779@gmail.com>
2018-12-12 17:06:10 +0000

    Validation for signup password

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,17 +1,19 @@
 // Validation for sign-up
-const password = document.getElementById('password');
-const confirmPassword = document.getElementById('confirm-password');
-
 const submitBtn = document.getElementById('sign-up-submit');
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    // e.preventDefault();
+    console.log(password);
+    console.log(confirmPassword);
     if (password !== confirmPassword) {
         console.log('Passwords do not match');
         const error = document.createElement('p');
         error.textContent = 'Passwords do not match';
         document.getElementById('sign-up-form').appendChild(error);
-        //confirmPassword.setCustomValidity('Passwords do not match');
+        document.getElementById('password').style.border = '2px red solid';
+        document.getElementById('confirm-password').style.border = '2px red solid';
     } else {
-        console.log('Lalala')
+        console.log('Passwords do match! Logged in!')   
     }
 });
